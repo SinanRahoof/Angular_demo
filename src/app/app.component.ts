d190a993aefb9e8ab2228d295ebf23b14d827474
Sinan Rahoof <sinanrahoof@Sinans-MacBook-Pro.local>
2020-11-03 20:58:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GetApiService} from './get-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstangularapp';
+  title = 'This should be my first angular application';
+
+  li:any;
+  lis=[];
+  first_name:string;
+  last_name:string;
+  email:string;
+
+  constructor(private api:GetApiService)
+  {
+
+  }
+  ngOnInit()
+  {
+    this.api.apiCall().subscribe((val) => {
+      console.warn("get api data",val);
+      this.li=val;
+      this.lis=this.li.data;
+    });
+  }
+  searchfname()
+  {
+    if(this.first_name != "")
+    {
+      this.lis = this.lis.filter(res=>{
+        return res.first_name.toLocaleLowerCase().match(this.first_name.toLocaleLowerCase());
+      });
+    }else if(this.first_name == "")
+    {
+      this.ngOnInit();
+    }
+    
+  }
+
+  searchlname()
+  {
+    if(this.last_name != "")
+    {
+      this.lis = this.lis.filter(res=>{
+        return res.last_name.toLocaleLowerCase().match(this.last_name.toLocaleLowerCase());
+      });
+    }else if(this.last_name == "")
+    {
+      this.ngOnInit();
+    }
+  }
+  
+  searchemail()
+  {
+    if(this.email != "")
+    {
+      this.lis = this.lis.filter(res=>{
+        return res.email.toLocaleLowerCase().match(this.email.toLocaleLowerCase());
+      });
+    }else if(this.email == "")
+    {
+      this.ngOnInit();
+    }
+  }
+
 }
